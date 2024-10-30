66a45438f03b5f39fb62a3713171d28f8314c858
wh0crypt <wh0crypt@protonmail.com>
2024-10-30 20:31:02 +0000

    Added style to the message
    Signed-off-by: wh0crypt <wh0crypt@protonmail.com>

--- profile/ssh-login-notify.sh
+++ profile/ssh-login-notify.sh
@@ -3,9 +3,20 @@
 # Bot that controls the SSH Login activities on the server
 
 login_ip="$(echo $SSH_CONNECTION | cut -d " " -f 1)"
-login_date="$(date +"%e %b %Y, %a %r")"
+login_date="$(date +"%e %b %Y")"
+login_time="$(date +", %a %r")"
 login_name="$(whoami)"
 
-message="New SSH login to server"$'\n'"$login_name"$'\n'"$login_ip"$'\n'"$login_date"
+message=$(cat <<EOF
+**New SSH Login to server:**
+───────────────────────────────
+Username: __"$login_name"__
+IP: __"$login_ip"__
+Date: __"$login_date"__
+Time: __"$login_time"__
+───────────────────────────────
+🔐 **__Please ensure you recognize this login.__**
+EOF
+)
 
 telegram-send "$message"
