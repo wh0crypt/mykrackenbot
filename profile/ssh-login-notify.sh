4846864141f3a58a457b0363e4b5f4f95c15796e
wh0crypt <wh0crypt@protonmail.com>
2024-11-01 15:53:13 +0000

    Fixed parsing
    Signed-off-by: wh0crypt <wh0crypt@protonmail.com>

--- profile/ssh-login-notify.sh
+++ profile/ssh-login-notify.sh
@@ -7,18 +7,15 @@ login_user="$(whoami)"
 login_date="$(date +"%e %b %Y")"
 login_time="$(date +", %a %r")"
 
-message=$(cat <<EOF
+message="""
 **New SSH Login to server:**
-───────────────────────────────
+\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─
 Username: __"$login_user"__
 IP: __"$login_ip"__
 Date: __"$login_date"__
 Time: __"$login_time"__
-───────────────────────────────
-🔐 **__Please ensure you recognize this login.__**
-EOF
-)
+\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─\─
+🔐 **__Please ensure you recognize this login____\.__**
+"""
 
-encoded_message=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$message'''))")
-
-telegram-send.py "/root/mykrackenbot/.env" "$encoded_message"
+telegram-send.py "/root/mykrackenbot/.env" "$message"
